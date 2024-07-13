f54f317a597b87d9290238457696b29a201d217a
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2024-07-13 21:19:50 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app.models import User, AdminMessage
+from app.models import User, History
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -12,6 +12,6 @@ class UserAdmin(admin.ModelAdmin):
             queryset |= self.model.objects.filter(chat_id=search_term)
         return queryset.distinct(), use_distinct  # adding distinct()
 
-@admin.register(AdminMessage)
-class AdminMessageAdmin(admin.ModelAdmin):
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
     pass
