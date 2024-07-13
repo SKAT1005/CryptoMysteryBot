ca2e7d78895b0d5fd7da189576dedf20c787cb09
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2024-07-13 19:30:45 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,21 +2,15 @@ from django.contrib import admin
 
 from app.models import User, AdminMessage
 
-class Chat_ID_Filter(admin.SimpleListFilter):
-    title = 'Фильтрация по ID телеграмма'
-    parameter_name = 'ID телеграмма'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('Дмитрий', 'Дмитрий'),
-        )
-
-    def queryset(self, request, queryset):
-        value = self.value()
-        return queryset.filter(chat_id='394151925')
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_filter = (Chat_ID_Filter,)
+    list_display = ['chat_id']
+    search_fields = ['chat_id']
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if search_term != '':
+            queryset |= self.model.objects.filter(chat_id=search_term)
+        return queryset.distinct(), use_distinct  # adding distinct()
 
 @admin.register(AdminMessage)
 class AdminMessageAdmin(admin.ModelAdmin):
